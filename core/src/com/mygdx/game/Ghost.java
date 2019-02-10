eb74021d6b7589062ccb1e7dd796cf23678dd911
adeef <feeda1539@gmail.com>
2019-02-10 16:26:53 -0500

    asdlfm

--- core/src/com/mygdx/game/Ghost.java
+++ core/src/com/mygdx/game/Ghost.java
@@ -7,20 +7,31 @@ import java.util.ArrayList;
 
 public class Ghost {
     private int x = 13*24;
-    private int y = 19*24;
+    private int y = 16*24;
+
+
     private String state = "right";
     private int prevAx;
     private int prevAy;
     private int PacX;
     private int PacY;
+
     private boolean scared = false;
     private boolean eaten = false;
+
+
     private int movement = 2;//how much a ghost changes position
     private int animation_count=0;
     private int frame=1;
+
+    public boolean canGive = false;
+
     Timer t = new Timer();
-    
-    
+
+
+    private ArrayList<String> shortCuts = new ArrayList<String>();
+
+
     private String color;
     private int[][] level = {
             {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
@@ -37,7 +48,7 @@ public class Ghost {
             {4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 2, 4, 4, 2, 4, 4, 4, 2, 4},
             {4, 6, 2, 2, 6, 4, 4, 6, 2, 2, 6, 4, 4, 0, 4, 4, 4, 4, 4, 6, 0, 0, 6, 4, 4, 6, 2, 2, 2, 6, 4},
             {4, 2, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4},
-            {4, 2, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 4, 0, 6, 0, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4},
+            {4, 2, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 4, 0, 4, 6, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4},
             {4, 6, 2, 2, 6, 4, 4, 6, 2, 2, 6, 4, 4, 0, 4, 4, 4, 4, 4, 6, 0, 0, 6, 4, 4, 6, 2, 2, 2, 6, 4},
             {4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 2, 4, 4, 2, 4, 4, 4, 2, 4},
             {4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 2, 4, 4, 2, 4, 0, 4, 2, 4},
@@ -52,13 +63,10 @@ public class Ghost {
             {4, 6, 2, 2, 6, 4, 4, 6, 2, 2, 6, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 6, 2, 2, 6, 2, 2, 2, 6, 4},
             {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4}
     };
-    
+
     public Ghost(String color){
-        
+
         this.color = color;
-        if(color.equals("blue")){
-            x = 15*24;
-        }
     }
 
     public Texture ghost_pic(){
@@ -81,17 +89,18 @@ public class Ghost {
         }
         return pic;
     }
-    
+
     public int[] move(int PacX,int PacY){
         isEaten();
-        
-        if(eaten && x/24 == 1 && y/24 == 1){
+
+        if(eaten && x/24 == 1 && y/24 == 1){//once home is reached
             scared = false;
             eaten = false;
         }
         if(eaten){
             return middle();
         }
+
         t.stop();
         if(scared && (int)t.getElapsedTimeSecs() > 10){
             scared = false;
@@ -104,13 +113,23 @@ public class Ghost {
         }
         this.PacX = PacX;
         this.PacY = PacY;
+
+
         if(color.equals("blue")){
             int[] pos = new int[2];
             int Ax = x / 24;
             int Ay = y / 24;
-            
+
             state = shortestPath(PacX/24,PacY/24);//calculate shortest path
-            
+            //shortP(new ArrayList<String>(),x/24,y/24,PacX/24,PacY/24);
+            //System.out.println(shortCuts);
+
+
+            if(state.equals("")){
+                state = "up";
+            }
+
+
             if (state.equals("right")) {
                 x += movement;
             } else if (state.equals("left")) {
@@ -126,12 +145,14 @@ public class Ghost {
             if (x >= 648) {
                 x = 24;
             }
-            
+
             prevAx = Ax;
             prevAy = Ay;
-            
+
+
             pos[0] = x / 24 * 24;//rounding position to match a 24x24 box
-            pos[1] = y / 24 * 24; 
+            pos[1] = y / 24 * 24;
+
             return pos;
         }
 
@@ -140,7 +161,6 @@ public class Ghost {
         int Ay = y / 24;
 
         if (level[Ax][Ay] == 6 && (Ax != prevAx || Ay != prevAy)) {//checks if ghost is at a junction and new position isnt the old one
-            
             state = PosChoice().get((int)(Math.random()*(PosChoice().size())));//finding possible directions and choosing one
         }
 
@@ -167,15 +187,46 @@ public class Ghost {
         pos[0] = x / 24 * 24;//rounding position to match a 24x24 box
         pos[1] = y / 24 * 24;
         return pos;
-        
+
+    }
+    //recursive path finding - :( did not work
+    private void shortP(ArrayList<String> paths, int x1, int y1, int x2, int y2 ){
+        System.out.println(x1+","+y1);
+        if(x1 == x2 && y1 == y2){
+            if(paths.size() < shortCuts.size()){
+                shortCuts = paths;
+            }
+        }
+
+        ArrayList<String> possible = PosChoice();
+        System.out.println(possible);
+
+        for(int i = 0; i < possible.size(); i++){
+
+            System.out.println(possible.get(i));
+            paths.add(possible.get(i));
+
+            if(possible.get(i).equals("right")){
+                shortP(paths,x1+1,y1,x2,y2);
+            }
+            if(possible.get(i).equals("left")){
+                shortP(paths,x1-1,y1,x2,y2);
+            }
+            if(possible.get(i).equals("up")){
+                shortP(paths,x1,y1+1,x2,y2);
+            }
+            if(possible.get(i).equals("down")){
+                shortP(paths,x1,y1-1,x2,y2);
+            }
+        }
     }
 
-    private String shortestPath(int x2, int y2) {
+    private String shortestPath(int x2, int y2) {//A-star path finding algorithm
         ArrayList<String> possible = PosChoice();
         double dist = Double.POSITIVE_INFINITY;//value of shortest path in possible
         String path = "";//direction ghost will move in
-        
-        
+
+
         for(int i = 0; i < possible.size(); i++){
             int Ax = x/24;
             int Ay = y/24;
@@ -222,7 +273,7 @@ public class Ghost {
         ArrayList<String> choices = new ArrayList<String>();
         int Ax = x/24;
         int Ay = y/24;
-        
+
         if(Ax < 30) {//checking possibilities
             if (level[Ax + 1][Ay] != 4 && level[Ax + 1][Ay] != 5) {
                 choices.add("right");
@@ -243,7 +294,7 @@ public class Ghost {
                 choices.add("down");
             }
         }
-        
+
         if(choices.size() > 1){//avoids going back and forth(removing opposite direction)
             if(state .equals("left")) {
                 choices.remove("right");
@@ -258,7 +309,7 @@ public class Ghost {
                 choices.remove("up");
             }
         }
-        
+
 
         return choices;
     }
@@ -266,45 +317,33 @@ public class Ghost {
         scared = true;
         t = new Timer();
     }
-    public boolean data_send(){
-        boolean out;
-        if(x/24 == PacX/24 && y/24 == PacY/24){
-            out=true;
-        }
-        else {
-            out=false;
-        }
-        return out;
-    }
-    public boolean scared_send(){
-        return scared;
-    }
 
-    
+
     public void isEaten(){
         if(!eaten){
             if(x/24 == PacX/24 && y/24 == PacY/24 && scared){
                 eaten = true;
+                canGive = true;
 
-                System.out.println("wowow");
             }
         }
-
     }
+
+
     public int[] middle(){//when pacman eats a ghost while they're scared
         state = shortestPath(1,1);
         int[] pos = new int[2];
         int Ax = x / 24;
         int Ay = y / 24;
-        
+
         if (state.equals("right")) {
-            x += movement*2;
+            x += movement*5;
         } else if (state.equals("left")) {
-            x -= movement*2;
+            x -= movement*5;
         } else if (state.equals("up")) {
-            y += movement*2;
+            y += movement*5;
         } else if (state.equals("down")) {
-            y -= movement*2;
+            y -= movement*5;
         }
         if (x <= 24) {//allows for side teleportation 
             x = 647;
@@ -316,6 +355,8 @@ public class Ghost {
         prevAx = Ax;
         prevAy = Ay;
 
+
+
         pos[0] = x / 24 * 24;//rounding position to match a 24x24 box
         pos[1] = y / 24 * 24;
         return pos;
@@ -324,7 +365,5 @@ public class Ghost {
         boolean touch = (x/24 == PacX/24 && y/24 == PacY/24 && !scared);
         return touch;
     }
-    public boolean eaten_check(){
-        return eaten;
-    }
+
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,36 +16,33 @@ import java.util.*;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture pac;
-	Texture Main_Menu;
-	Texture you_win;
-	Texture play_btn;
-	Texture play_btn_hover;
-	Texture backgroundTexture;
-	Texture backgroundTexture_win;
-	BitmapFont font;
-	Texture life;
-	Texture dot;
-	Texture big_dot;
-	BitmapFont time;
-	
-	ShapeRenderer sr;
-	Texture redG;
+    SpriteBatch batch;
+    Texture pac;
+    Texture Main_Menu;
+    Texture play_btn;
+    Texture play_btn_hover;
+    Texture backgroundTexture;
+    BitmapFont font;
+    Texture life;
+    Texture dot;
+    Texture big_dot;
+    BitmapFont time;
 
-	int Px=0;
-	int Py=0;
+    ShapeRenderer sr;
+    Texture redG;
 
-	boolean start=true;
+    int Px=0;
+    int Py=0;
 
-	int backg_animation_count;
-	int total_backg_animation_count;
-	
-	int Rx = 0;//red ghost coords
-	int Ry = 0;
-	
-	int Bx = 0;//blue ghost coords
-	int By = 0;
+    boolean start=true;
+
+
+
+    int Rx = 0;//red ghost coords
+    int Ry = 0;
+
+    int Bx = 0;//blue ghost coords
+    int By = 0;
 
     int Ox = 0;//orange ghost coords
     int Oy = 0;
@@ -54,19 +51,19 @@ public class MyGdxGame extends ApplicationAdapter {
     int Piy = 0;
 
     int[][] pos = {{Rx,Ry},{Bx,By},{Pix,Piy},{Ox,Oy}};
-    
+
     int lives = 3;
-    String main_time="";
+
     Ghost red = new Ghost("red");
     Ghost blue = new Ghost("blue");
-	Ghost pink = new Ghost("pink");
-	Ghost orange = new Ghost("orange");
+    Ghost pink = new Ghost("pink");
+    Ghost orange = new Ghost("orange");
 
     Ghost[] ghosts = new Ghost[4];
 
-	
-	String state = "RIGHT";//pacmans state
-	Pacman p = new Pacman();
+
+    String state = "RIGHT";//pacmans state
+    Pacman p = new Pacman();
 
 
     Timer t = new Timer();
@@ -75,176 +72,99 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
     @Override
-	public void create () {
-		Gdx.graphics.setWindowedMode(672, 800);
-		backg_animation_count=0;
-		total_backg_animation_count=0;
-		batch = new SpriteBatch();
-		backgroundTexture = new Texture("fullLevel.png");
-		backgroundTexture_win= new Texture("fullLevel_winner.png");
-		pac = new Texture("Original_PacMan_RIGHT1.png");
-		Main_Menu= new Texture("Main_Menu.png");
-		play_btn=new Texture("Play_button.png");
-		play_btn_hover=new Texture("Play_button_Hover.png");
-		redG = new Texture("redL.png");
-		dot=new Texture("point1.png");
+    public void create () {
+        Gdx.graphics.setWindowedMode(672, 800);
+
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture("fullLevel.png");
+        pac = new Texture("Original_PacMan_RIGHT1.png");
+        Main_Menu= new Texture("Main_Menu.png");
+        play_btn=new Texture("Play_button.png");
+        play_btn_hover=new Texture("Play_button_Hover.png");
+        redG = new Texture("redL.png");
+        dot=new Texture("point1.png");
         big_dot=new Texture("big_point1.png");
-		font = new BitmapFont(Gdx.files.internal("joystix.fnt"),Gdx.files.internal("joystix.png"),false);
-		time = new BitmapFont(Gdx.files.internal("joystix.fnt"),Gdx.files.internal("joystix.png"),false);
-        you_win = new Texture("you_win.png");
-		sr = new ShapeRenderer();
-		
-		life = new Texture("Original_PacMan_RIGHT1.png");
-		
-		
+        font = new BitmapFont(Gdx.files.internal("joystix.fnt"),Gdx.files.internal("joystix.png"),false);
+        time = new BitmapFont(Gdx.files.internal("joystix.fnt"),Gdx.files.internal("joystix.png"),false);
 
-	}
+        sr = new ShapeRenderer();
 
-    public void total_reset(){
-        Px=0;
-        Py=0;
-
-        backg_animation_count=0;
-        total_backg_animation_count=0;
-
-        Rx = 0;//red ghost coords
-        Ry = 0;
-
-        Bx = 0;//blue ghost coords
-        By = 0;
-
-        Ox = 0;//orange ghost coords
-        Oy = 0;
-
-        Pix = 0;//pink ghost coords
-        Piy = 0;
-        lives = 3;
-        String main_time="";
-        red = new Ghost("red");
-        blue = new Ghost("blue");
-        pink = new Ghost("pink");
-        orange = new Ghost("orange");
-
-        ghosts = new Ghost[4];
-
-
-        state = "RIGHT";//pacmans state
-        p.points_reset();
-        p = new Pacman();
-
-
-        t = new Timer();
+        life = new Texture("Original_PacMan_RIGHT1.png");
 
 
 
     }
 
-	@Override
-	public void render () {
 
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    @Override
+    public void render () {
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         if(start){
             starting_menu();
         }
-        
+
         else{
 
             batch.draw(backgroundTexture,0,0);
-            if((p.winner() || lives==0) && total_backg_animation_count<=120){
 
-                if(backg_animation_count>=12){
-                    batch.draw(backgroundTexture_win,0,0);
-                }
-                if(backg_animation_count>=24){
-                    backg_animation_count=0;
-                }
-                backg_animation_count+=1;
-                total_backg_animation_count+=1;
-            }
-            if(total_backg_animation_count==121){
-                batch.draw(Main_Menu,0,0);
-                if(p.winner()){
-                    batch.draw(you_win,186,400);
-                }
-                if(lives==0){
+            state = p.state();
+            pac=p.pac_pic();
 
+            movement();//moves player and ghosts
 
-                }
+            drawO();
 
-                batch.draw(play_btn,224,240);
-                int mx = Gdx.input.getX();
-                int my = Gdx.input.getY();
+            scared();
 
-                if(mx>=224 && mx<=449 && my>=502 && my<=560){
-                    batch.draw(play_btn_hover,224,240);
-                    if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-                        System.out.println("IT RESET HEREEEE");
-                        total_reset();
+            timer();
 
+            batch.draw(pac, Px, Py);
+
+            for(int i = 0; i < 4; i++){
+                if(ghosts[i].touchPac()){
+                    lives -=1;
+                    reset();
+                    try{
+                        Thread.sleep(2000);
                     }
-                }
-
-
-            }
-            else {
-                state = p.state();
-                pac=p.pac_pic();
-                if(!p.winner()){
-                    movement();//moves player and ghosts
-
-                }
-
-                drawO();
-
-                scared();
-
-                timer();
-
-
-                batch.draw(pac, Px, Py);
-
-                for(int i = 0; i < 4; i++){
-                    if(ghosts[i].touchPac()){
-                        lives -=1;
-                        reset();
-                        break;
-                    }
-                }
-
-
-
-                for(int i = 0; i <lives; i++){
-                    batch.draw(life,500+i*35,760);
+                    catch (Exception e){}
+                    break;
                 }
             }
 
 
-            if(!start){
 
-                ArrayList<int[]>drawDots = p.drawing(2);
-
-                for(int i = 0; i < drawDots.size(); i++){
-                    batch.draw(dot,drawDots.get(i)[0]*24+12,drawDots.get(i)[1]*24+12);
-                    //             sr.rect(drawDots.get(i)[0]*24+12,drawDots.get(i)[1]*24+12,4,4);
-                }
-
-
-                drawDots = p.drawing(3);
-
-                // sr.begin(ShapeType.Filled);
-                //sr.setColor(1,1,1,0);
-                for(int i = 0; i < drawDots.size(); i++){
-                    batch.draw(big_dot,drawDots.get(i)[0]*24+8,drawDots.get(i)[1]*24+8);
-                    //sr.rect(drawDots.get(i)[0]*24+6,drawDots.get(i)[1]*24+6,12,12);
-                }
-
-
-                //sr.end();
+            for(int i = 0; i <lives; i++){
+                batch.draw(life,500+i*35,760);
             }
         }
         batch.end();
+
+        if(!start){
+
+            ArrayList<int[]>drawDots = p.drawing(2);
+            batch.begin();
+            for(int i = 0; i < drawDots.size(); i++){
+                batch.draw(dot,drawDots.get(i)[0]*24+12,drawDots.get(i)[1]*24+12);
+                //             sr.rect(drawDots.get(i)[0]*24+12,drawDots.get(i)[1]*24+12,4,4);
+            }
+
+
+            drawDots = p.drawing(3);
+            // sr.begin(ShapeType.Filled);
+            //sr.setColor(1,1,1,0);
+            for(int i = 0; i < drawDots.size(); i++){
+                batch.draw(big_dot,drawDots.get(i)[0]*24+8,drawDots.get(i)[1]*24+8);
+                //sr.rect(drawDots.get(i)[0]*24+6,drawDots.get(i)[1]*24+6,12,12);
+            }
+            batch.end();
+
+            //sr.end();
+        }
 
     }
 
@@ -253,10 +173,11 @@ public class MyGdxGame extends ApplicationAdapter {
     public void starting_menu(){
         batch.draw(Main_Menu,0,0);
         batch.draw(play_btn,224,371);
-        
+
         int mx=Gdx.input.getX();
         int my=Gdx.input.getY();
-        
+        System.out.println("X:"+mx+" Y:"+my);
+
         if(mx>=224 && mx<=449 && my>=371 && my<=429){
             batch.draw(play_btn_hover,224,371);
             if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
@@ -265,60 +186,51 @@ public class MyGdxGame extends ApplicationAdapter {
             }
         }
     }
-    
+
     public void movement(){
         ghosts[0] = red;
         ghosts[1] = blue;
         ghosts[2] = pink;
         ghosts[3] = orange;
-        
+
+
 
         Px = p.move()[0];
         Py = p.move()[1];
-        
+
         for(int i = 0; i < ghosts.length; i++){
             pos[i][0] = ghosts[i].move(Px,Py)[0];
             pos[i][1] = ghosts[i].move(Px,Py)[1];
         }
     }
-    
+
     public void timer(){
-        if(blue.data_send() && blue.scared_send()){
-            p.points_add(100);
+
+        for(int i = 0; i < 4; i++){
+            if(ghosts[i].canGive){
+                p.points_add(200);
+                ghosts[i].canGive = false;
+            }
         }
-        if(red.data_send() && red.scared_send()){
-            p.points_add(100);
-        }
-        if(pink.data_send() && pink.scared_send()){
-            p.points_add(100);
-        }
-        if(orange.data_send() && orange.scared_send()){
-            p.points_add(100);
-        }
+
+
         String points = "SCORE: "+p.grub()[0];
         font.draw(batch, points, 0,788);
         t.stop();
-        
+
         int secs = (int)t.getElapsedTimeSecs();
-        if(p.winner() && main_time.length()==0){
-           main_time = t.time_converter(secs);
-        }
+
         String time_display= t.time_converter(secs);
-        if(p.winner()){
-            time_display=main_time;
-        }
         String elap = "TIME: "+time_display;
         time.draw(batch,elap,250,788);
     }
 
-
-
-    
     public void drawO(){//drawing ghost positions
         batch.draw(red.ghost_pic(),pos[0][0],pos[0][1]);
-        batch.draw(blue.ghost_pic(),pos[1][0],pos[1][1]);
         batch.draw(pink.ghost_pic(),pos[2][0],pos[2][1]);
         batch.draw(orange.ghost_pic(),pos[3][0],pos[3][1]);
+        batch.draw(blue.ghost_pic(),pos[1][0],pos[1][1]);
+
 
     }
     public void scared(){
@@ -338,15 +250,15 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
         p.reset();
-        
-        
+
+
     }
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		pac.dispose();
-		font.dispose();
-		sr.dispose();
-	}
+    @Override
+    public void dispose () {
+        batch.dispose();
+        pac.dispose();
+        font.dispose();
+        sr.dispose();
+    }
 }
